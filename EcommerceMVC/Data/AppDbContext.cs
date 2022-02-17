861f14accf85599a7bc9786947b7481720bd88f4
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-02-16 23:13:48 -0300

    upd dbcontext

--- EcommerceMVC/Data/AppDbContext.cs
+++ EcommerceMVC/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EcommerceMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceMVC.Data
 {
@@ -8,5 +9,34 @@ namespace EcommerceMVC.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorID,
+                am.MovieID
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie)
+                                              .WithMany(am => am.Actors_Movies)
+                                              .HasForeignKey(m => m.MovieID);
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor)
+                                             .WithMany(am => am.Actors_Movies)
+                                             .HasForeignKey(m => m.ActorID);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
+
+
+
     }
 }
