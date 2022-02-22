0ab7d6c9d46cc9fa70db3bbef5c7e24561e1252b
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-02-22 01:14:57 -0300

    upd movies and navbar

--- EcommerceMVC/Controllers/MoviesController.cs
+++ EcommerceMVC/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using EcommerceMVC.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EcommerceMVC.Controllers
@@ -16,7 +17,10 @@ namespace EcommerceMVC.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movies = await _context.Movies.ToListAsync();
+            var movies = await _context.Movies
+                                .Include(c => c.Cinema)
+                                .OrderByDescending(m => m.StartDate).ThenByDescending(m => m.Price)
+                                .ToListAsync();
 
             return View(movies);
         }
