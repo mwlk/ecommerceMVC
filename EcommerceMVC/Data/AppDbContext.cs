a0d5decf20f4ab2792090e2bf1edcf4d247943af
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-03-13 05:15:22 -0300

    upd dbcontext

--- EcommerceMVC/Data/AppDbContext.cs
+++ EcommerceMVC/Data/AppDbContext.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceMVC.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
