922a7b534077156e0e4d26eec9af67cb88df862b
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-03-13 17:40:53 -0300

    add orders controller

---
+++ EcommerceMVC/Controllers/OrdersController1.cs
@@ -0,0 +1,34 @@
+﻿using EcommerceMVC.Data.Cart;
+using EcommerceMVC.Data.Interface_Services;
+using EcommerceMVC.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Controllers
+{
+    public class OrdersController1 : Controller
+    {
+        private readonly IMovieService _service;
+        private readonly ShoppingCart _cart;
+
+        public OrdersController1(IMovieService service, ShoppingCart cart)
+        {
+            _service = service;
+            _cart = cart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _cart.GetShoppingCartItems();
+
+            _cart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartViewModel()
+            {
+                Shopping = _cart,
+                CartTotal = _cart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ EcommerceMVC/Data/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EcommerceMVC.Data.Cart;
+
+namespace EcommerceMVC.Data.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart Shopping { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
