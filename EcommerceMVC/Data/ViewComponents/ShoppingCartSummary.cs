32f605ee42620fd5ac557ad229cc5d635acccc57
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-03-13 22:52:15 -0300

    add cart summary

---
+++ EcommerceMVC/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceMVC.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _cart;
+
+        public ShoppingCartSummary(ShoppingCart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _cart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
